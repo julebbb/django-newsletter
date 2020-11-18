0b8e266762e430c0efb46a4f8b92e2905c65872c
Nicolas CAN <nicolas.can@univ-lille.fr>
2020-11-18 15:31:40 +0100

    reorganise migrations

--- newsletter/migrations/0007_message_edito.py
+++ newsletter/migrations/0007_message_edito.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('newsletter', '0004_auto_20180407_1043'),
+        ('newsletter', '0006_auto_20190417_1106'),
     ]
 
     operations = [
